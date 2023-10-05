8051bbfee7408b90aa08bd7487b8c25724ad345c
Beomsoo Son <sbs@henge.io>
2023-10-05 12:18:37 +0900

    Add GTag

--- components/Container.js
+++ components/Container.js
@@ -1,6 +1,8 @@
+import Head from "next/head";
 import { Components } from "./Framer/Components";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import Script from "next/script";
 
 export function Container({ children }) {
   const router = useRouter();
@@ -35,19 +37,35 @@ export function Container({ children }) {
   }
 
   return (
-    <div className="container">
-      <Components.Navbar
-        home={() => router.push("/")}
-        about={() => router.push("/about")}
-        viewport={() => router.push("/viewport")}
-        hydro={() => router.push("/hydro")}
-        ess={() => router.push("/ess")}
-        material={() => router.push("/material")}
-        className="width_100 sticky"
-        variant={navbarVariant} // Set the variant dynamically
-      />
-      {children}
-      <Components.Footer className="width_100" />
-    </div>
+    <>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          <!-- Google tag (gtag.js) -->
+          <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11361899346">
+          </script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'AW-11361899346');
+          </script>
+        `}
+      </Script>
+      <div className="container">
+        <Components.Navbar
+          home={() => router.push("/")}
+          about={() => router.push("/about")}
+          viewport={() => router.push("/viewport")}
+          hydro={() => router.push("/hydro")}
+          ess={() => router.push("/ess")}
+          material={() => router.push("/material")}
+          className="width_100 sticky"
+          variant={navbarVariant} // Set the variant dynamically
+        />
+        {children}
+        <Components.Footer className="width_100" />
+      </div>
+    </>
   );
 }
